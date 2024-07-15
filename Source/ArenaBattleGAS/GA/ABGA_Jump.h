621691f7ee0b7b91583b2497f6eaead2fc7fe2a3
yuyu0830 <yuyu0830@naver.com>
2024-07-15 13:43:54 +0900

    5강

---
+++ Source/ArenaBattleGAS/GA/ABGA_Jump.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "ABGA_Jump.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLEGAS_API UABGA_Jump : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UABGA_Jump();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+protected:
+	UFUNCTION()
+	void OnLandedCallback();
+};
